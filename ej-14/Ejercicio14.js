7fe9edc68794fc01dac88ee63cc6c508efb76557
Marina <uo257247@uniovi.es>
2021-11-30 18:22:04 +0100

    Arreglo de errores ej 13 para taw

--- ej-14/Ejercicio14.js
+++ ej-14/Ejercicio14.js
@@ -1,31 +1,25 @@
 class FileManager {
 
-    checkSupport(){
+    checkSupport() {
         if (window.File && window.FileReader && window.FileList && window.Blob) {
-            //El navegador soporta el API File
+
             console.log("<p>Este navegador soporta el API File </p>");
-        }
-        else console.log("<p>¡¡¡ Este navegador NO soporta el API File y este programa puede no funcionar correctamente !!!</p>");
+        } else console.log("<p>¡¡¡ Este navegador NO soporta el API File y este programa puede no funcionar correctamente !!!</p>");
     }
 
     leerArchivoTexto(files) {
-        //Solamente toma un archivo
-        //var archivo = document.getElementById("archivoTexto").files[0];
         var archivo = files[0];
-        //Solamente admite archivos de tipo texto
+        //Solo archivos de texto
         var tipoTexto = /text.*/;
-        if (archivo.type.match(tipoTexto) || archivo.type=="application/json") {
+        if (archivo.type.match(tipoTexto) || archivo.type == "application/json") {
             var lector = new FileReader();
-            lector.onload = function (evento) {
-                //El evento "onload" se lleva a cabo cada vez que se completa con éxito una operación de lectura
-                //La propiedad "result" es donde se almacena el contenido del archivo
-                //Esta propiedad solamente es válida cuando se termina la operación de lectura
-                var postit = $("<div id='list"+(g.n++)+"' class='list' draggable='true' ondragstart='g.dragStart(event)'></div>").text(lector.result);
+            lector.onload = function(evento) {
+
+                var postit = $("<div id='list" + (g.n++) + "' class='list' draggable='true' ondragstart='g.dragStart(event)'></div>").text(lector.result);
                 $("#last").append(postit);
             }
             lector.readAsText(archivo);
-        }
-        else {
+        } else {
             errorArchivo.innerText = "Error : ¡¡¡ Archivo no válido !!!";
         }
     }
@@ -37,18 +31,18 @@ fm.checkSupport();
 
 class ArrangeGame {
 
-    constructor(){
+    constructor() {
         this.n = 0;
     }
     dragStart(event) {
         event.dataTransfer.setData("text/plain", event.target.id);
     }
 
-    allowDrop(event){
+    allowDrop(event) {
         event.preventDefault();
     }
 
-    drop(event){
+    drop(event) {
         event.preventDefault();
         const data = event.dataTransfer.getData("text/plain");
         const element = document.querySelector(`#${data}`);
@@ -92,8 +86,8 @@ class MapaDinamicoGoogle {
         }
     }
 
-    initGoogleMap(){
-        var oviedo = { lat: this.latitud, lng: this.longitud};
+    initGoogleMap() {
+        var oviedo = { lat: this.latitud, lng: this.longitud };
         var mapaOviedo = new google.maps.Map(document.getElementById('mapa'), { zoom: 8, center: oviedo });
         var marcador = new google.maps.Marker({ position: oviedo, map: mapaOviedo });
     }
@@ -101,4 +95,3 @@ class MapaDinamicoGoogle {
 }
 
 var mapaDinamicoGoogle = new MapaDinamicoGoogle();
-
