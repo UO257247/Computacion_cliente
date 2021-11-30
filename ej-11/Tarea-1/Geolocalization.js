e030c55ba95578cda023103ae83da8d4a6159eab
Marina <uo257247@uniovi.es>
2021-11-30 12:37:03 +0100

    Arreglado Tarea-1 Ej 11

--- ej-11/Tarea-1/Geolocalization.js
+++ ej-11/Tarea-1/Geolocalization.js
@@ -1,37 +1,37 @@
 "use strict";
-class Geolocalización {
-    constructor (){
+class Geolocalizacion {
+    constructor() {
         navigator.geolocation.getCurrentPosition(this.getPosicion.bind(this));
     }
-    getPosicion(posicion){
-        this.longitud         = posicion.coords.longitude; 
-        this.latitud          = posicion.coords.latitude;  
-        this.precision        = posicion.coords.accuracy;
-        this.altitud          = posicion.coords.altitude;
+    getPosicion(posicion) {
+        this.longitud = posicion.coords.longitude;
+        this.latitud = posicion.coords.latitude;
+        this.precision = posicion.coords.accuracy;
+        this.altitud = posicion.coords.altitude;
         this.precisionAltitud = posicion.coords.altitudeAccuracy;
-        this.rumbo            = posicion.coords.heading;
-        this.velocidad        = posicion.coords.speed;       
+        this.rumbo = posicion.coords.heading;
+        this.velocidad = posicion.coords.speed;
     }
-    getLongitud(){
+    getLongitud() {
         return this.longitud;
     }
-    getLatitud(){
+    getLatitud() {
         return this.latitud;
     }
-    getAltitud(){
+    getAltitud() {
         return this.altitud;
     }
-    verTodo(dondeVerlo){
-        var ubicacion=document.getElementById(dondeVerlo);
-        var datos=''; 
-        datos+='<p>Longitud: '+this.longitud +' grados</p>'; 
-        datos+='<p>Latitud: '+this.latitud +' grados</p>';
-        datos+='<p>Precisión de la latitud y longitud: '+ this.precision +' metros</p>';
-        datos+='<p>Altitud: '+ this.altitude +' metros</p>';
-        datos+='<p>Precisión de la altitud: '+ this.precisionAltitud +' metros</p>'; 
-        datos+='<p>Rumbo: '+ this.rumbo +' grados</p>'; 
-        datos+='<p>Velocidad: '+ this.velocidad +' metros/segundo</p>';
+    verTodo(dondeVerlo) {
+        var ubicacion = document.getElementById(dondeVerlo);
+        var datos = '';
+        datos += '<p>Longitud: ' + this.longitud + ' grados</p>';
+        datos += '<p>Latitud: ' + this.latitud + ' grados</p>';
+        datos += '<p>Precisión de la latitud y longitud: ' + this.precision + ' metros</p>';
+        datos += '<p>Altitud: ' + this.altitude + ' metros</p>';
+        datos += '<p>Precisión de la altitud: ' + this.precisionAltitud + ' metros</p>';
+        datos += '<p>Rumbo: ' + this.rumbo + ' grados</p>';
+        datos += '<p>Velocidad: ' + this.velocidad + ' metros/segundo</p>';
         ubicacion.innerHTML = datos;
     }
 }
-var miPosicion = new Geolocalización();
+var miPosicion = new Geolocalizacion();
