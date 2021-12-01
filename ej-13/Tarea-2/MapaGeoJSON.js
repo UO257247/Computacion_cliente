b3b94028c091465f7bf14d27b440f1a373b8fcc2
Marina <uo257247@uniovi.es>
2021-12-01 01:46:24 +0100

    Tarea 2 ej 13

--- ej-13/Tarea-2/MapaGeoJSON.js
+++ ej-13/Tarea-2/MapaGeoJSON.js
@@ -1,25 +1,49 @@
 'use strict';
 
 class MapaGeoJSON {
-    constructor() {}
-
-    init() {
-        const file = document.getElementById("files").files[0];
-        if (file.name.includes('.geoJSON')) {
-            const reader = new FileReader();
-            reader.onloadend = () => {
-                const map = new google.maps.Map(document.getElementById('map'), {
-                    zoom: 11,
-                    center: { lat: 43.30, lng: -5.9 }
-                });
-                const json = JSON.parse(reader.result);
-                map.data.addGeoJson(json);
-            };
-            reader.readAsText(file);
-        } else {
-            alert("Error");
+    constructor() {
+        if (!window.File && window.FileReader && window.FileList && window.Blob) {
+            console.log("El navegador no soporta la API File!");
         }
+    }
+
+    checkFileApi() {
+        if (!window.File && window.FileReader && window.FileList && window.Blob) {
+            $("#map").html("<p>El navegador no soporta la API File!</p>");
+        }
+    }
+
+    loadFile() {
+        console.log("Loading file...");
+
+        let file = $("#fileInput")[0].files[0];
+
+        if (!file.name.includes('.geoJSON')) {
+            $("#map").html("<p>Archivo inválido!</p>");
+            return;
+        }
+
+        let reader = new FileReader();
+        reader.onload = () => {
+            console.log("File loaded!");
+
+            let map = new google.maps.Map($("#map")[0], {
+                zoom: 11,
+                center: {
+                    lat: 43.30,
+                    lng: -5.9
+                }
+            });
+            let json = JSON.parse(reader.result);
+            map.data.addGeoJson(json);
+        };
+        console.log("Reading file...");
+        reader.readAsText(file, "UTF-8");
     }
 }
 
-const map = new MapaGeoJSON();
+var geoJson = new MapaGeoJSON();
+
+$(document).ready(function() {
+    geoJson.checkFileApi();
+})
